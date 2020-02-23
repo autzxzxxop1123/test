9c6d2dca7d1ff26c0beabd362b5c50a6456e7afc
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2020-02-23 20:33:59 +0700

    Add files via upload

---
+++ test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	
+	
+)
+
+func main(){
+	var searchPath string
+	var typeFile string
+	fmt.Println("Please Enter Your SearchPath")
+	fmt.Scanln(&searchPath)
+	fmt.Println("Please Enter Your typeFile")
+	fmt.Scanln(&typeFile)
+
+	
+}
+
