8294b47d7bc8a603f917c002a5e32a6f9b60869e
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2020-02-23 19:47:33 +0700

    Add files via upload

---
+++ create.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+)
+
+func main(){
+	file, err := os.Create("output.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteString("Hello \n")
+	file.WriteString("output.txt")
+}
